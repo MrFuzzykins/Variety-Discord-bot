c4eec8958b2147b895f10b30267a835c43abe938
MrFuzzykins <shawn.huttonism@gmail.com>
2020-06-13 14:52:44 +1000

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
- const client = new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
-client.login('token');
+client.login('NzIxMTkyMDE3ODExNjAzNDk3.XuRAuQ.XSGVmVNONgnpW7rCxIWQTrSPF-M');
